31f0cde74763c0c3b886b3bcc395c91064303898
WojcikDariusz <dariusz.wojcik@op.pl>
2021-10-25 17:21:29 +0200

    Minor changes and rearangement

---
+++ server/index.js
@@ -0,0 +1,55 @@
+/*
+ * -------------------------------
+ * Copyright © 2021 Dariusz Wójcik
+ * -------------------------------
+ */
+
+"use strict";
+
+// Load required modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+
+// Adding CORS verification
+const appConfig = require("./app/config/app.config.js");
+
+let corsOrigin = {
+  origin: `http://${appConfig.HOST}:${appConfig.ORIGIN_PORT}`
+};
+app.use(cors(corsOrigin));
+
+// Load module responsible for routes handling
+require("./app/routes/public.routes.js")(app);
+
+// Load database models and config
+const db = require("./app/models");
+const dbConfig = require("./app/config/db.config.js");
+
+// Connect to mongoDB
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //After resolving db.mongoosse.connect invoke the initial function
+    console.log("Connection to mongoDB established.");
+  })
+  .catch((err) => {
+    console.error("Connection error with mongoDB", err);
+    //Terminate node.js app when connection to mongoDB error occurs
+    process.exit();
+  });
+
+  module.exports = app
--- server/server.js
+++ server/server.js
@@ -1,56 +1,5 @@
-/*
- * -------------------------------
- * Copyright © 2021 Dariusz Wójcik
- * -------------------------------
- */
-
-"use strict";
-
-// Load required modules
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const app = express();
-
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-app.use(cors());
-
-// Adding CORS verification
+const app = require("./index");
 const appConfig = require("./app/config/app.config.js");
-
-let corsOrigin = {
-  origin: `http://${appConfig.HOST}:${appConfig.ORIGIN_PORT}`
-};
-app.use(cors(corsOrigin));
-
-// Load module responsible for routes handling
-require("./app/routes/public.routes.js")(app);
-
-// Load database models and config
-const db = require("./app/models");
-const dbConfig = require("./app/config/db.config.js");
-
-// Connect to mongoDB
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    //After resolving db.mongoosse.connect invoke the initial function
-    console.log("Connection to mongoDB established.");
-  })
-  .catch((err) => {
-    console.error("Connection error with mongoDB", err);
-    //Terminate node.js app when connection to mongoDB error occurs
-    process.exit();
-  });
 
 // Listen for incoming requests
 const PORT = process.env.PORT || `${appConfig.PORT}`;
